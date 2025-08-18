442c08a5a9b8fe58dd7e266874550adc60dd866b
ben99933 <yangben999@gmail.com>
2025-08-19 02:10:36 +0800

    fix(commonNavbar): add key in v-for

--- src/components/commonNavbar.vue
+++ src/components/commonNavbar.vue
@@ -1,6 +1,6 @@
 <template>
 <ul class="navbar-list  justify-start items-start">
-    <li class="navbar-item" v-for="item in commonNavbarItems">
+    <li class="navbar-item" v-for="item in commonNavbarItems" :key="item.name">
         <a :href="item.link">
             <button class="navbar-link hover:text-blue-500" data-nav-link>
                 {{item.name}}
@@ -21,6 +21,7 @@ class CommonNavbarItem {
 const commonNavbarItems:CommonNavbarItem[]=[];
 commonNavbarItems.push(new CommonNavbarItem("Blog", "#/blog/home"));
 commonNavbarItems.push(new CommonNavbarItem("About Me", "#/about/AboutMe"));
+if(import.meta.env.DEV)commonNavbarItems.push(new CommonNavbarItem("Admin", "#/admin"));
 
 
 
